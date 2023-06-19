7959954d810a0bc427e1d832290b750d3398c5c9
Albert Karagulov <albert.karagulov@mail.ru>
2023-06-19 13:56:16 +0300

    4.0

--- src/App.js
+++ src/App.js
@@ -2,40 +2,22 @@ import React from 'react';
 import './scss/app.scss';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-	const [items, setItems] = React.useState([]);
-
-	React.useEffect(() => {
-		fetch('https://648f35e875a96b664444d7d0.mockapi.io/items')
-			.then(res => {
-				return res.json();
-			})
-			.then(arr => {
-				setItems(arr);
-			});
-	}, []);
-
 	return (
 		<div className='App'>
 			<div className='wrapper'>
 				<Header />
 				<div className='content'>
-					<div className='container'>
-						<div className='content__top'>
-							<Categories />
-							<Sort />
-						</div>
-						<h2 className='content__title'>Все пиццы</h2>
-						<div className='content__items'>
-							{items.map(obj => (
-								<PizzaBlock key={obj.id} {...obj} />
-							))}
-						</div>
-					</div>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
 				</div>
 			</div>
 		</div>
