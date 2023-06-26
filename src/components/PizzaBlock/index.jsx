21adda62354f5e65f1e5393e24dd271737096682
Albert Karagulov <albert.karagulov@mail.ru>
2023-06-26 17:09:21 +0300

    16.0

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-function PizzaBlock({ title, price, imageUrl, sizes, types }) {
-	const [pizzaCount, setPizzaCount] = React.useState(0);
+import { addItem } from '../../redux/slices/cartSlice';
+
+const typeNames = ['тонкое', 'традиционное'];
+
+function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
+	const dispatch = useDispatch();
+	const cartItem = useSelector(state =>
+		state.cart.items.find(obj => obj.id === id)
+	);
 	const [activeType, setActiveType] = React.useState(0);
 	const [activeSize, setActiveSize] = React.useState(0);
 
-	const typeNames = ['тонкое', 'традиционное'];
+	const addedCount = cartItem ? cartItem.count : 0;
 
 	const onClickAdd = () => {
-		setPizzaCount(pizzaCount + 1);
+		const item = {
+			id,
+			title,
+			price,
+			imageUrl,
+			type: typeNames[activeType],
+			size: sizes[activeSize]
+		};
+		dispatch(addItem(item));
 	};
 
 	return (
@@ -59,7 +75,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
 							/>
 						</svg>
 						<span>Добавить</span>
-						<i>{pizzaCount}</i>
+						{addedCount > 0 && <i>{addedCount}</i>}
 					</button>
 				</div>
 			</div>
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
 	categoryId: 0,
-	currentPage: 0,
+	currentPage: 1,
 	sort: {
 		name: 'популярности',
 		sortProperty: 'rating'
