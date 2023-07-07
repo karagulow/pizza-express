150fb8f9915ac0ec0dcef27491340537ac1c9dd2
Albert Karagulov <albert.karagulov@mail.ru>
2023-07-07 15:02:47 +0300

    23.0

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -5,26 +5,24 @@ import styles from './Pagination.module.scss';
 
 type PaginationProps = {
 	currentPage: number;
-	onChangePage: any;
+	onChangePage: (page: number) => void;
 };
 
 const Pagination: React.FC<PaginationProps> = ({
 	currentPage,
 	onChangePage
-}) => {
-	return (
-		<ReactPaginate
-			className={styles.root}
-			breakLabel='...'
-			nextLabel='>'
-			previousLabel='<'
-			onPageChange={event => onChangePage(event.selected + 1)}
-			pageRangeDisplayed={8}
-			pageCount={2}
-			forcePage={currentPage - 1}
-			// renderOnZeroPageCount={null}
-		/>
-	);
-};
+}) => (
+	<ReactPaginate
+		className={styles.root}
+		breakLabel='...'
+		nextLabel='>'
+		previousLabel='<'
+		onPageChange={event => onChangePage(event.selected + 1)}
+		pageRangeDisplayed={8}
+		pageCount={2}
+		forcePage={currentPage - 1}
+		// renderOnZeroPageCount={null}
+	/>
+);
 
 export default Pagination;
